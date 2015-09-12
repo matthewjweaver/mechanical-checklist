2c3a47d705831ef1dcf92a64402f07ad26d56abe
matthew j weaver <matt@otheym.mjw.wtf>
2015-09-12 16:55:43 -0400

    Add bcrypt. Because crypto.

--- mechanical_checklist/settings.py
+++ mechanical_checklist/settings.py
@@ -100,3 +100,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
